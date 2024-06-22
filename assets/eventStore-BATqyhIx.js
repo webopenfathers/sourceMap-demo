ffb8b6038879a65e093a4556eba4623aae00ee7e
webopenfathers <webopenfathers@users.noreply.github.com>
2024-06-22 14:57:06 +0000

    自动部署 94d0fa7237c83e4a7bb955fc95e9582a2f2e765f

---
+++ assets/eventStore-BATqyhIx.js
@@ -0,0 +1,2 @@
+import{g as t}from"./index-Cmhnvw6X.js";const s=t("event",{state:()=>({events:[]}),actions:{addEvent(e){this.events=e},getEvent(){return this.events}}});export{s as u};
+//# sourceMappingURL=eventStore-BATqyhIx.js.map
