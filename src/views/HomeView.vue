c52935aa690ee8fbc3e9b2bddb3229b230fe6ace
zwx1132021 <1290443925@qq.com>
2024-06-22 19:46:16 +0800

    2024 6-22 19:45 改造homeView页面，显示具体错误信息

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+import { log } from 'util'
+
+let js_error = ref<any>([])
+let isError = ref(false)
+
+onMounted(() => {
+  try {
+    let jsErrorList = localStorage.getItem('jsErrorList')
+    if (jsErrorList) {
+      isError.value = true
+      js_error.value = JSON.parse(jsErrorList)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div v-if="isError">
+    <pre>
+      {{ js_error.stack }}
+    </pre>
+  </div>
 </template>
