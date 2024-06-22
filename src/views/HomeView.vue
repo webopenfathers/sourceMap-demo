343b25eb132d01a4ba06a0835d7f8890e4acdbc1
zwx1132021 <1290443925@qq.com>
2024-06-22 20:04:36 +0800

    2024 6-22 20:04 改造错误定位页面的逻辑

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
+<template>
+  <div v-if="isError">
+    <el-collapse v-model="activeName" accordion>
+      <el-collapse-item
+        v-for="(item, index) in js_error.stack_frames"
+        :key="index"
+        :title="item.source"
+        :name="index"
+      >
+        <el-row>
+          <el-col :span="20">
+            <div>{{ item.fileName }}</div>
+          </el-col>
+          <el-col :span="4">
+            <el-button type="primary" size="small" @click="openDialog(item, index)">
+              映射源码
+            </el-button>
+          </el-col>
+        </el-row>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+</template>
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { log } from 'util'
 
 let js_error = ref<any>([])
 let isError = ref(false)
+let activeName = ref(0)
 
 onMounted(() => {
   try {
@@ -16,12 +39,6 @@ onMounted(() => {
     console.log(error)
   }
 })
-</script>
 
-<template>
-  <div v-if="isError">
-    <pre>
-      {{ js_error.stack }}
-    </pre>
-  </div>
-</template>
+const openDialog = () => {}
+</script>
